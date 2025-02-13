2b1f900557cf888d28e81c45c72b1bc541ea86fe
Lucas Marques <lucsilva@pagar.me>
2025-02-13 16:34:20 -0300

    :sparkles: feat: add sign up route

---
+++ src/MyMoney.Api/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using MyMoney.Api.ApiModels.Response;
+using MyMoney.Application.UseCases.Authentication.Commands.SignUp;
+using MyMoney.Application.UseCases.Authentication.Common;
+
+namespace MyMoney.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/auth")]
+public class AuthController(IMediator mediator) : ApiController
+{
+    [HttpPost("sign-up")]
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(ApiResponse<AuthResult>), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> Register(
+        [FromBody] SignUpCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        var output = await mediator.Send(request, cancellationToken);
+        return Ok(output);
+    }
+}
