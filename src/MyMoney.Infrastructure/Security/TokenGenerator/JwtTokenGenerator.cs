24a605124f667cd6aed9de1d60d7beed695b3105
Lucas Marques <lucsilva@pagar.me>
2025-02-13 16:25:23 -0300

    :sparkles: feat: add jwt token generator

---
+++ src/MyMoney.Infrastructure/Security/TokenGenerator/JwtTokenGenerator.cs
@@ -0,0 +1,36 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MyMoney.Application.Intefaces;
+
+namespace MyMoney.Infrastructure.Security.TokenGenerator;
+
+public class JwtTokenGenerator(IOptions<JwtSettings> jwtSettings) : IJwtTokenGenerator
+{
+    private readonly JwtSettings _jwtSettings = jwtSettings.Value;
+
+    public string GenerateToken(Guid id, string name, string email)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Name, name),
+            new(JwtRegisteredClaimNames.Email, email),
+            new("id", id.ToString()),
+        };
+
+        var token = new JwtSecurityToken(
+            _jwtSettings.Issuer,
+            _jwtSettings.Audience,
+            claims,
+            expires: DateTime.UtcNow.AddMinutes(_jwtSettings.TokenExpirationInMinutes),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
