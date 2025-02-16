4e35259fcd0d8da6d367017b5cef8e577821b52a
Lucas Marques <lucas.msilva09@gmail.com>
2025-02-16 15:02:13 -0300

    :hammer: refactor: remove property from auth result

--- src/MyMoney.Application/UseCases/Authentication/Common/AuthResult.cs
+++ src/MyMoney.Application/UseCases/Authentication/Common/AuthResult.cs
@@ -2,14 +2,8 @@ using MyMoney.Domain.Entities;
 
 namespace MyMoney.Application.UseCases.Authentication.Common;
 
-public record AuthResult(
-    Guid Id,
-    string Name,
-    string Email,
-    DateTime CreatedAt,
-    string Token
-)
+public record AuthResult(Guid Id, string Name, string Email, string Token)
 {
-    public static AuthResult GenerateResult(User user, string token)
-        => new(user.Id, user.Name, user.Email, user.CreatedAt, token);
+    public static AuthResult GenerateResult(User user, string token) =>
+        new(user.Id, user.Name, user.Email, token);
 }
