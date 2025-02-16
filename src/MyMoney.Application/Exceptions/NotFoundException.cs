75c302583cf3e91ab819f416b095ddbc03656e58
Lucas Marques <lucas.msilva09@gmail.com>
2025-02-16 15:04:06 -0300

    :sparkles: feat: add not found exception

---
+++ src/MyMoney.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+namespace MyMoney.Application.Exceptions;
+
+public class NotFoundException(string message) : ApplicationException(message)
+{
+    public static void ThrowIfNull(object? @object, string exceptionMessage)
+    {
+        if (@object is null)
+            throw new NotFoundException(exceptionMessage);
+    }
+}
