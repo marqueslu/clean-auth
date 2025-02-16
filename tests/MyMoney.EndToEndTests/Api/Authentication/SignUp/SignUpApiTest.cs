c3bf06e6c184e44008e14be11538c365aaf53b07
Lucas Marques <lucas.msilva09@gmail.com>
2025-02-16 18:32:14 -0300

    :shirt: refactor: adjust identation

--- tests/MyMoney.EndToEndTests/Api/Authentication/SignUp/SignUpApiTest.cs
+++ tests/MyMoney.EndToEndTests/Api/Authentication/SignUp/SignUpApiTest.cs
@@ -17,12 +17,12 @@ public class SignUpApiTest(SignUpApiTestFixture fixture) : IDisposable
         response.Content.Email.ShouldBe(command.Email);
         response.Content.Token.ShouldNotBeNull();
     }
-    
+
     [Fact]
     public async Task Given_SignUp_When_AllIncorrectDataIsPassed_Then_ShouldReturnStatusCode422()
     {
         var command = fixture.GetInvalidCommand();
-        
+
         var response = await fixture.ApiClient.SignUpAsync(command);
         response.ShouldNotBeNull();
         response.StatusCode.ShouldBe(HttpStatusCode.UnprocessableEntity);
@@ -34,13 +34,11 @@ public class SignUpApiTest(SignUpApiTestFixture fixture) : IDisposable
         var command = fixture.GetCommand();
         var user = fixture.GetValidUserFromCommand(command.Name, command.Email, command.Password);
         await fixture.Persistence.Save(user);
-        
+
         var response = await fixture.ApiClient.SignUpAsync(command);
         response.ShouldNotBeNull();
         response.StatusCode.ShouldBe(HttpStatusCode.Conflict);
     }
 
-    public void Dispose()
-        =>
-            fixture.CleanPersistence();
+    public void Dispose() => fixture.CleanPersistence();
 }
