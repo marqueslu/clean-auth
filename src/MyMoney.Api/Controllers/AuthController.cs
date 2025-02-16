b2de90f98b33968af623b28a8a44b57c498bf4f7
Lucas Marques <lucas.msilva09@gmail.com>
2025-02-16 15:05:31 -0300

    :sparkles: feat: add sign in api

--- src/MyMoney.Api/Controllers/AuthController.cs
+++ src/MyMoney.Api/Controllers/AuthController.cs
@@ -1,11 +1,10 @@
 using MediatR;
-
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
 using MyMoney.Api.ApiModels.Response;
 using MyMoney.Application.UseCases.Authentication.Commands.SignUp;
 using MyMoney.Application.UseCases.Authentication.Common;
+using MyMoney.Application.UseCases.Authentication.Queries.SignIn;
 
 namespace MyMoney.Api.Controllers;
 
@@ -20,6 +19,20 @@ public class AuthController(IMediator mediator) : ApiController
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
     public async Task<IActionResult> Register(
         [FromBody] SignUpCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        var output = await mediator.Send(request, cancellationToken);
+        return Ok(output);
+    }
+
+    [HttpPost("sign-in")]
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(ApiResponse<AuthResult>), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> SignIn(
+        [FromBody] SignInQuery request,
         CancellationToken cancellationToken
     )
     {
